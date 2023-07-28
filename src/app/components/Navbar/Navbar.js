271bf961ec73d29b91184e029bc70b19f5bad0c6
ebrardev <16155052@mersin.edu.tr>
2023-07-28 18:59:02 +0300

    img

---
+++ src/app/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            
+            <img src='/img/Union.png' alt='logo' className='logo' />
+            <h1>favolio</h1>
+        </div>
+    );
+}
