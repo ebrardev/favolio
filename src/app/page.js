1fa93610f823fba0d6035291ae5fe998c1e59896
ebrardev <16155052@mersin.edu.tr>
2023-07-28 18:59:17 +0300

    logo add

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Navbar from './components/Navbar/Navbar'
 
 export default function Home() {
   return (
   <div>
-  <h1>favolio</h1>
+   <Navbar/>
   </div>
   )
 }
